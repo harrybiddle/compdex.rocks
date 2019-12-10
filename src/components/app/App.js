115d37a4897a99bc2f04ef57a2e649ea09a20dd5
Harry Biddle <harrybiddle@users.noreply.github.com>
2019-12-10 22:25:14 +0100

    Working drag example

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from '../../logo.svg';
 import './App.css';
+import Column from "../column/Column";
+import { DragDropContext } from "react-beautiful-dnd";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const initialData = {
+    tasks: {
+      'task-1': {id: "task-1", content: 'Take out the garbage' },
+      'task-2': {id: "task-2", content: 'Feed the cat' },
+    },
+    columns: {
+      "column-1": {
+        id: "column-1",
+        title: "to-do",
+        taskIds: ["task-1", "task-2"]
+      },
+    },
+    columnOrder: ["column-1"]
+  }
+
+// Next: https://egghead.io/lessons/react-persist-list-reordering-with-react-beautiful-dnd-using-the-ondragend-callback
+class App extends React.Component {
+  state = initialData;
+
+  onDragEnd = result => {
+
+  }
+
+  render() {
+    return (
+      <DragDropContext
+          onDragEnd={this.onDragEnd}
+      >
+        {
+          this.state.columnOrder.map(columnId => {
+            const column = this.state.columns[columnId];
+            const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
+
+            return <Column key={column.id} column={column} tasks={tasks} />;
+          })
+        }
+      </DragDropContext>)
+  }
 }
 
 export default App;
---
+++ src/components/column/Column.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Task from "../task/Task";
+import { Droppable } from "react-beautiful-dnd"
+
+export default class Column extends React.Component {
+    render() {
+        return (
+            <div>
+                <h3>{this.props.column.title}</h3>
+                <Droppable droppableId={this.props.column.id}>
+                    {provided => (
+                        <div
+                            ref={provided.innerRef}
+                            {...provided.droppableProps}
+                        >
+                            {this.props.tasks.map((task, index) => <Task key={task.id} task={task} index={index} />)}
+                            {provided.placeholder}
+                        </div>
+                    )}
+                </Droppable>
+            </div>
+        );
+    }
+}
