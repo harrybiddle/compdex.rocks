20d8b10c854c313ba26e37c8650e2e5563a7e3bb
Harry Biddle <harrybiddle@users.noreply.github.com>
2019-12-27 11:39:12 +0100

    Persist reorder

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,26 +3,48 @@ import "./App.css";
 import Column from "../column/Column";
 import { DragDropContext } from "react-beautiful-dnd";
 
-const initialData = {
-  tasks: {
-    "task-1": { id: "task-1", content: "Take out the garbage" },
-    "task-2": { id: "task-2", content: "Feed the cat" }
-  },
-  columns: {
-    "column-1": {
-      id: "column-1",
-      title: "to-do",
-      taskIds: ["task-1", "task-2"]
-    }
-  },
-  columnOrder: ["column-1"]
-};
-
 // Next: https://egghead.io/lessons/react-persist-list-reordering-with-react-beautiful-dnd-using-the-ondragend-callback
 class App extends React.Component {
-  state = initialData;
+  state = {
+    tasks: {
+      "task-1": { id: "task-1", content: "Adam Ondra" },
+      "task-2": { id: "task-2", content: "Alex Megos" }
+    },
+    columns: {
+      "column-1": {
+        id: "column-1",
+        title: "Speed Round",
+        taskIds: ["task-1", "task-2"]
+      }
+    },
+    columnOrder: ["column-1"]
+  };
 
-  onDragEnd = result => {};
+  onDragEnd = result => {
+    const { destination, source, draggableId } = result;
+
+    if (!destination) return;
+
+    const sourceColumn = this.state.columns[source.droppableId];
+    const newTaskIds = Array.from(sourceColumn.taskIds);
+    newTaskIds.splice(source.index, 1); // remove source
+    newTaskIds.splice(destination.index, 0, draggableId); // insert destination
+
+    const newColumn = {
+      ...sourceColumn,
+      taskIds: newTaskIds
+    };
+
+    const newState = {
+      ...this.state,
+      columns: {
+        ...this.state.columns,
+        [newColumn.id]: newColumn
+      }
+    };
+
+    this.setState(newState);
+  };
 
   render() {
     return (
