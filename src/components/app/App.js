7e02aab797abbf42e41ed2df6dab4a3b20c13e76
Harry Biddle <harrybiddle@users.noreply.github.com>
2019-12-29 19:41:04 +0100

    Pull up list state into top-level component (#6)
    * Simplify state dictionary. Fix warnings.
    
    * Move up state into App component

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,17 +3,72 @@ import "./App.css";
 import KnownResults from "../knownresults/KnownResults";
 import Predictions from "../predictions/Predictions";
 
+import update from "immutability-helper";
+
 class App extends React.Component {
-  state = {};
+  state = {
+    columns: {
+      "column-1": {
+        title: "Speed Round",
+        athletes: [
+          { id: "athlete-1", content: "Adam Ondra" },
+          { id: "athlete-2", content: "Alex Megos" }
+        ]
+      },
+      "column-2": {
+        title: "Isolation Zone",
+        athletes: [{ id: "athlete-3", content: "Margo Hayes" }]
+      }
+    },
+    columnOrder: ["column-1", "column-2"]
+  };
+
+  onDragEnd = result => {
+    const newState = newStateOnDragEnd(this.state, result);
+    this.setState(newState);
+  };
 
   render() {
     return (
       <div>
         <Predictions />
-        <KnownResults />
+        <KnownResults
+          onDragEnd={this.onDragEnd}
+          columns={this.state.columns}
+          columnOrder={this.state.columnOrder}
+        />
       </div>
     );
   }
+}
+
+export function newStateOnDragEnd(oldState, result) {
+  const { destination, source } = result;
+  if (!destination) return oldState;
+
+  // fetch athlete
+  const athlete = oldState.columns[source.droppableId].athletes[source.index];
+
+  // remove athlete from source column
+  let newState = oldState;
+  newState = update(newState, {
+    columns: {
+      [source.droppableId]: {
+        athletes: { $splice: [[source.index, 1]] }
+      }
+    }
+  });
+
+  // insert athlete into target column
+  newState = update(newState, {
+    columns: {
+      [destination.droppableId]: {
+        athletes: { $splice: [[destination.index, 0, athlete]] }
+      }
+    }
+  });
+
+  return newState;
 }
 
 export default App;
--- src/components/knownresults/KnownResults.js
+++ src/components/knownresults/KnownResults.js
@@ -1,71 +1,16 @@
 import React from "react";
 import Column from "../column/Column";
 import { DragDropContext } from "react-beautiful-dnd";
-import update from "immutability-helper";
-
-export function newStateOnDragEnd(oldState, result) {
-  const { destination, source, draggableId } = result;
-  if (!destination) return oldState;
-
-  // remove droppable from source column
-  let newState = oldState;
-  newState = update(newState, {
-    columns: {
-      [source.droppableId]: {
-        athleteIds: { $splice: [[source.index, 1]] }
-      }
-    }
-  });
-
-  // insert droppable into target column
-  newState = update(newState, {
-    columns: {
-      [destination.droppableId]: {
-        athleteIds: { $splice: [[destination.index, 0, draggableId]] }
-      }
-    }
-  });
-
-  return newState;
-}
 
 class KnownResults extends React.Component {
-  state = {
-    athletes: {
-      "athlete-1": { id: "athlete-1", content: "Adam Ondra" },
-      "athlete-2": { id: "athlete-2", content: "Alex Megos" },
-      "athlete-3": { id: "athlete-3", content: "Margo Hayes" }
-    },
-    columns: {
-      "column-1": {
-        id: "column-1",
-        title: "Speed Round",
-        athleteIds: ["athlete-1", "athlete-2"]
-      },
-      "column-2": {
-        id: "column-2",
-        title: "Isolation Zone",
-        athleteIds: ["athlete-3"]
-      }
-    },
-    columnOrder: ["column-1", "column-2"]
-  };
-
-  onDragEnd = result => {
-    const newState = newStateOnDragEnd(this.state, result);
-    this.setState(newState);
-  };
-
   render() {
     return (
-      <DragDropContext onDragEnd={this.onDragEnd}>
-        {this.state.columnOrder.map(columnId => {
-          const column = this.state.columns[columnId];
-          const athletes = column.athleteIds.map(
-            athleteId => this.state.athletes[athleteId]
+      <DragDropContext onDragEnd={this.props.onDragEnd}>
+        {this.props.columnOrder.map(columnId => {
+          const column = this.props.columns[columnId];
+          return (
+            <Column key={columnId} droppableId={columnId} column={column} />
           );
-
-          return <Column key={column.id} column={column} athletes={athletes} />;
         })}
       </DragDropContext>
     );
