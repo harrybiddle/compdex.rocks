11afc67b49816e240fedcd1a1b903a259c8bb8cd
Harry Biddle <harrybiddle@users.noreply.github.com>
2020-01-03 12:15:26 +0100

    Basic grid layout

--- src/components/predictions/Predictions.js
+++ src/components/predictions/Predictions.js
@@ -3,24 +3,26 @@ import React from "react";
 class Predictions extends React.Component {
   render() {
     return (
-      <table>
-        <thead>
-          <tr>
-            {Object.keys(this.props.columns).map((name, index) => (
-              <th key={index}>{name}</th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {this.props.rows.map((row, index) => (
-            <tr key={index}>
-              {row.map((column, index_) => (
-                <td key={index_}>{column}</td>
+      <div id="probabilities-container">
+        <table>
+          <thead>
+            <tr>
+              {Object.keys(this.props.columns).map((name, index) => (
+                <th key={index}>{name}</th>
               ))}
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {this.props.rows.map((row, index) => (
+              <tr key={index}>
+                {row.map((column, index_) => (
+                  <td key={index_}>{column}</td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     );
   }
 }
