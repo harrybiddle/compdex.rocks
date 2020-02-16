583d634bc72f74b24a16a38c36a9f5d50a05f0bb
Harry Biddle <harrybiddle@users.noreply.github.com>
2020-02-16 21:04:58 +0100

    Remove unused code

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -7,31 +7,19 @@ import {
   Route,
   Redirect
 } from "react-router-dom";
-import update from "immutability-helper";
 
-export default class App extends React.Component {
-  state = {
-    splashScreen: true
-  };
-
-  clearSplash() {
-    const newState = { splashScreen: { $set: false } };
-    this.setState(update(this.state, newState));
-  }
-
-  render() {
-    return (
-      <Router>
-        <Switch>
-          <Route path="/comp">
-            <Competition />
-          </Route>
-          <Route path="/">
-            <Redirect to="/" />
-            <Splash to="/comp" />
-          </Route>
-        </Switch>
-      </Router>
-    );
-  }
+export default function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/comp">
+          <Competition />
+        </Route>
+        <Route path="/">
+          <Redirect to="/" />
+          <Splash to="/comp" />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
