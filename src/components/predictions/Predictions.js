32df6857674be276de974d412e95972df7cadb9c
Harry Biddle <harrybiddle@users.noreply.github.com>
2020-02-23 20:30:48 +0100

    promises

--- src/components/predictions/Predictions.js
+++ src/components/predictions/Predictions.js
@@ -4,7 +4,7 @@ import { arrayDifference } from "../../common/utils";
 import { stages } from "../constants";
 import { probabilities } from "../../common/bruteforce";
 import { calculateCentreOfMass } from "../competition/Competition";
-import { isEqual } from "lodash";
+import { useAsync } from "react-async";
 
 export function ridgelinePlotProps(predictionsProps) {
   const athletes = Object.keys(predictionsProps.athletes);
@@ -28,19 +28,51 @@ export function ridgelinePlotProps(predictionsProps) {
     }, {})
   };
 }
+//
+// export default function Predictions(props) {
+//   // computationShouldProceed() {
+//   //   const allAthleteIds = Object.keys(this.props.athletes);
+//   //   const numberMissingAthletes = stage =>
+//   //     arrayDifference(allAthleteIds, this.props.stages[stage]).length;
+//   //   return (
+//   //     numberMissingAthletes(stages.QUALIFICATION) === 0 &&
+//   //     numberMissingAthletes(stages.SPEED) === 0 &&
+//   //     numberMissingAthletes(stages.BOULDER) < 2
+//   //   );
+//   // }
+//   //
+//   // shouldComponentUpdate = nextProps => !isEqual(this.props, nextProps);
+//   return <RidgelinePlot {...ridgelinePlotProps(props)} />;
+// }
 
-export default function Predictions(props) {
-  // computationShouldProceed() {
-  //   const allAthleteIds = Object.keys(this.props.athletes);
-  //   const numberMissingAthletes = stage =>
-  //     arrayDifference(allAthleteIds, this.props.stages[stage]).length;
-  //   return (
-  //     numberMissingAthletes(stages.QUALIFICATION) === 0 &&
-  //     numberMissingAthletes(stages.SPEED) === 0 &&
-  //     numberMissingAthletes(stages.BOULDER) < 2
-  //   );
-  // }
-  //
-  // shouldComponentUpdate = nextProps => !isEqual(this.props, nextProps);
-  return <RidgelinePlot {...ridgelinePlotProps(props)} />;
+// You can use async/await or any function that returns a Promise
+const loadPlayer = async ({ playerId }, { signal }) => {
+  return playerId;
+};
+
+function promiseFn({ props }) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      console.log("start promise");
+      resolve(ridgelinePlotProps(props));
+      console.log("finish promise");
+    }, 1000);
+  });
+}
+
+export default function MyComponent(props) {
+  const { data, error, isPending } = useAsync({
+    promiseFn: promiseFn,
+    props: props,
+    watch: JSON.stringify(props) // stringify due to shallow compare
+  });
+  console.log("render");
+
+  return (
+    <div>
+      {isPending && "Loading..."}
+      {error && `Something went wrong: ${error.message}`}
+      {data && <RidgelinePlot {...data} />}
+    </div>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/app/App";
 import * as serviceWorker from "./serviceWorker";
+import HelloWorker from "./predictions.worker.js";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
@@ -10,3 +11,23 @@ ReactDOM.render(<App />, document.getElementById("root"));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+const helloWorker = new HelloWorker();
+let messageCount = 0;
+
+helloWorker.postMessage({ run: true });
+
+helloWorker.onmessage = event => {
+  if (event.data.status) {
+    console.log("STATUS", event.data.status);
+  }
+
+  if (event.data.message) {
+    messageCount += 1;
+    console.log("MESSAGE", event.data.message);
+
+    if (messageCount >= 5) {
+      helloWorker.postMessage({ run: false });
+    }
+  }
+};
