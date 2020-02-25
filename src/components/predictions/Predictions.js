fd104442f1a9c7de0f017548c272a308119be38c
Harry Biddle <harrybiddle@users.noreply.github.com>
2020-02-25 07:31:55 +0100

    Working promises

--- src/components/predictions/Predictions.js
+++ src/components/predictions/Predictions.js
@@ -1,34 +1,9 @@
 import React from "react";
 import RidgelinePlot from "../ridgelineplot/RidgelinePlot";
-import { arrayDifference } from "../../common/utils";
-import { stages } from "../constants";
-import { probabilities } from "../../common/bruteforce";
-import { calculateCentreOfMass } from "../competition/Competition";
+import { useAsync } from "react-async";
 // eslint-disable-next-line import/no-webpack-loader-syntax
 import worker from "workerize-loader!../../predictions.worker";
 
-export function ridgelinePlotProps(predictionsProps) {
-  const athletes = Object.keys(predictionsProps.athletes);
-  let rows = Object.entries(
-    probabilities(
-      new Set(athletes),
-      predictionsProps.stages[stages.QUALIFICATION],
-      predictionsProps.stages[stages.SPEED],
-      predictionsProps.stages[stages.BOULDER],
-      predictionsProps.stages[stages.LEAD]
-    )
-  ).map(a => [predictionsProps.athletes[a[0]].name, a[1]]);
-  rows.sort(
-    (a, b) => calculateCentreOfMass(a[1]) - calculateCentreOfMass(b[1])
-  );
-  return {
-    athletes: rows.map(r => r[0]),
-    probabilities: rows.reduce((acc, value) => {
-      acc[value[0]] = value[1];
-      return acc;
-    }, {})
-  };
-}
 //
 // export default function Predictions(props) {
 //   // computationShouldProceed() {
@@ -61,25 +36,24 @@ export function ridgelinePlotProps(predictionsProps) {
 //   });
 // }
 
-export default function MyComponent(props) {
+function foo({ props }) {
   let inst = worker();
-  inst.burnCpu(1000).then(count => {
-    console.log(`Ran ${count} loops`);
+  return inst.ridgelinePlotProps(props);
+}
+
+export default function MyComponent(props) {
+  const { data, error, isPending } = useAsync({
+    promiseFn: foo,
+    props: props,
+    watch: JSON.stringify(props) // stringify due to shallow compare
   });
-  return <RidgelinePlot {...ridgelinePlotProps(props)} />;
-  //
-  // const { data, error, isPending } = useAsync({
-  //   promiseFn: promiseFn,
-  //   props: props,
-  //   watch: JSON.stringify(props) // stringify due to shallow compare
-  // });
-  // console.log("render");
-  //
-  // return (
-  //   <div>
-  //     {isPending && "Loading..."}
-  //     {error && `Something went wrong: ${error.message}`}
-  //     {data && <RidgelinePlot {...data} />}
-  //   </div>
-  // );
+  console.log("render, pending?" + isPending + " data=" + data);
+
+  return (
+    <div>
+      {isPending && "Loading..."}
+      {error && `Something went wrong: ${error.message}`}
+      {isPending || <RidgelinePlot {...data} />}
+    </div>
+  );
 }
