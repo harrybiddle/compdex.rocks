bcfb0cc363fe0dbbe71d5b1f2a9db67cd72a048b
Harry Biddle <harrybiddle@users.noreply.github.com>
2020-04-05 21:59:03 +0200

    Simplify with Bootstrap (#28)
    * Simplify using bootstrap
    
    * Pretty drag

--- src/components/listitem/ListItem.js
+++ src/components/listitem/ListItem.js
@@ -1,6 +1,16 @@
 import React from "react";
 import { Draggable } from "react-beautiful-dnd";
 import dragHandleIcon from "./drag_handle-24px.svg";
+import { ListGroup } from "react-bootstrap";
+
+const EXTRA_CLASSES_FOR_DRAGGING = [
+  "shadow",
+  "p-3",
+  "mb-5",
+  "bg-white rounded",
+  "border",
+  "border-light"
+];
 
 export default class ListItem extends React.Component {
   render() {
@@ -12,37 +22,23 @@ export default class ListItem extends React.Component {
             {...provided.dragHandleProps}
             ref={provided.innerRef}
           >
-            <div
-              style={{
-                fontSize: "14px",
-                height: "32px",
-                lineHeight: "1.43",
-                padding: "8px 16px 8px 16px",
-                // vertically center text
-                display: "flex",
-                alignItems: "center",
-                // ranked/unranked style
-                color: this.props.isRanked ? "black" : "#999",
-                // dragging style
-                ...(snapshot.isDragging
-                  ? {
-                      border: "1px solid #eee",
-                      backgroundColor: "#F5F5F5",
-                      boxShadow:
-                        "0px 2px 1px -1px rgba(0, 0, 0, 0.2),0px 1px 1px 0px rgba(0, 0, 0, 0.14),0px 1px 3px 0px rgba(0,0,0,.12)"
-                    }
-                  : {})
-              }}
+            <ListGroup.Item
+              variant={this.props.isRanked ? null : "light"}
+              className={[
+                "d-flex",
+                "justify-content-between",
+                "align-items-center",
+                ...(snapshot.isDragging ? EXTRA_CLASSES_FOR_DRAGGING : [])
+              ]}
             >
               <span style={{ width: "2.5ex" }}>{this.props.prefix}</span>
-
               {this.props.content}
               <img
                 src={dragHandleIcon}
                 alt="Drag handle icon"
                 style={{ marginLeft: "auto" }}
               />
-            </div>
+            </ListGroup.Item>
           </div>
         )}
       </Draggable>
