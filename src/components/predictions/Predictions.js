5be126e12f25739b0aff1d93327bac14404e8814
Harry Biddle <harrybiddle@users.noreply.github.com>
2020-04-13 20:17:53 +0200

    Put medal possibilities in cards

--- src/components/predictions/Predictions.js
+++ src/components/predictions/Predictions.js
@@ -1,19 +1,19 @@
 import React from "react";
-import RidgelinePlot from "../ridgelineplot/RidgelinePlot";
+import MedalScenarios from "../medalscenarios/MedalScenarios";
 import { useAsync } from "react-async";
 // eslint-disable-next-line import/no-webpack-loader-syntax
 import worker from "workerize-loader!./predictions.worker";
 //const worker = () => require("./predictions.worker");
 import Container from "react-bootstrap/Container";
 
-function generateScenarios({ props }) {
+function generateScenariosAsync({ props }) {
   let inst = worker();
   return inst.generateScenarios(props);
 }
 
 export default function Predictions(props) {
-  const { data, error, isPending } = useAsync({
-    promiseFn: generateScenarios,
+  const { data: scenarios, error, isPending } = useAsync({
+    promiseFn: generateScenariosAsync,
     props: props,
     watch: JSON.stringify(props) // stringify due to shallow compare
   });
@@ -21,7 +21,7 @@ export default function Predictions(props) {
     <Container>
       {isPending && "Loading..."}
       {error && `Something went wrong: ${error.message}`}
-      {isPending || <RidgelinePlot {...data} />}
+      {isPending || <MedalScenarios {...scenarios} />}
     </Container>
   );
 }
